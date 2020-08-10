519bd136a379acb9d54cab106d14e618e12c1e9e
Feng Hao <hiroushifuu@outlook.com>
2020-08-11 04:35:44 +0800

    [feat] implement draw 2 goals

--- frontend/admin.py
+++ frontend/admin.py
@@ -22,6 +22,8 @@ class ActorGoalAdmin(admin.ModelAdmin):
     change_list_template = 'change_list.html'
     list_display = [
         'actor',
+        'drawn_img_1_preview',
+        'drawn_img_2_preview',
         'goal_img_preview',
         'batch',
     ]
@@ -40,7 +42,7 @@ class ActorGoalAdmin(admin.ModelAdmin):
         actors = User.objects.all()
         # print(actors)
         GOALS = copy.deepcopy(BATTLE_GOALS)
-        drawn_goals = random.sample(GOALS, len(actors))
+        drawn_goals = random.sample(GOALS, len(actors) * 2)
         # print(drawn_goals)
         last_actor_goal = ActorGoal.objects.order_by('batch').last()
         if last_actor_goal is None:
@@ -48,21 +50,48 @@ class ActorGoalAdmin(admin.ModelAdmin):
         else:
             last_batch = last_actor_goal.batch + 1
         for index, actor in enumerate(actors):
-            ActorGoal.objects.create(actor=actor, goal_img_path=drawn_goals[index], batch=last_batch)
+            ActorGoal.objects.create(actor=actor, drawn_goal_1_img_path=drawn_goals[index * 2], drawn_goal_2_img_path=drawn_goals[index * 2 + 1], batch=last_batch)
         state = GoalState.objects.create(for_batch=last_batch, distributed=False)
         return HttpResponseRedirect('../')
 
     def goal_img_preview(self, obj):
         goal_id = 'goal_' + str(obj.id)
-        goal_name = obj.goal_img_path.split('/')[-1].split('.')[0]
-        # print(obj.goal_img_path, goal_name)
+        if obj.selected_goal_img_path:
+            goal_name = obj.selected_goal_img_path.split('/')[-1].split('.')[0]
+            # print(obj.selected_goal_img_path, goal_name)
+            return format_html(
+                '''
+                <img id="{}" style="display:none; position:absolute; top:50px; left:-50px); height:calc(297px); width:calc(210px); border:3px solid; z-index:1000;" src="{}" />
+                <div style="font-size:12pt; position:relative; cursor:pointer;" 
+                onmouseover="document.getElementById('{}').style.display='block'; position: 'relative'; document.getElementById('{}').style.left=this.getBoundingClientRect().left+'px'; document.getElementById('{}').style.cursor='pointer';"
+                onmouseout="document.getElementById('{}').style.display='none';">{}</div>
+                '''.format(goal_id, obj.selected_goal_img_path, goal_id, goal_id, goal_id, goal_id, goal_name))
+        else:
+            return ''
+
+    def drawn_img_1_preview(self, obj):
+        goal_id = 'goal_' + str(obj.id)
+        goal_name = obj.drawn_goal_1_img_path.split('/')[-1].split('.')[0]
+        # print(obj.drawn_goal_1_img_path, goal_name)
         return format_html(
             '''
             <img id="{}" style="display:none; position:absolute; top:50px; left:-50px); height:calc(297px); width:calc(210px); border:3px solid; z-index:1000;" src="{}" />
             <div style="font-size:12pt; position:relative; cursor:pointer;" 
             onmouseover="document.getElementById('{}').style.display='block'; position: 'relative'; document.getElementById('{}').style.left=this.getBoundingClientRect().left+'px'; document.getElementById('{}').style.cursor='pointer';"
             onmouseout="document.getElementById('{}').style.display='none';">{}</div>
-            '''.format(goal_id, obj.goal_img_path, goal_id, goal_id, goal_id, goal_id, goal_name))
+            '''.format(goal_id, obj.drawn_goal_1_img_path, goal_id, goal_id, goal_id, goal_id, goal_name))
+
+    def drawn_img_2_preview(self, obj):
+        goal_id = 'goal_' + str(obj.id)
+        goal_name = obj.drawn_goal_2_img_path.split('/')[-1].split('.')[0]
+        # print(obj.drawn_goal_2_img_path, goal_name)
+        return format_html(
+            '''
+            <img id="{}" style="display:none; position:absolute; top:50px; left:-50px); height:calc(297px); width:calc(210px); border:3px solid; z-index:1000;" src="{}" />
+            <div style="font-size:12pt; position:relative; cursor:pointer;" 
+            onmouseover="document.getElementById('{}').style.display='block'; position: 'relative'; document.getElementById('{}').style.left=this.getBoundingClientRect().left+'px'; document.getElementById('{}').style.cursor='pointer';"
+            onmouseout="document.getElementById('{}').style.display='none';">{}</div>
+            '''.format(goal_id, obj.drawn_goal_2_img_path, goal_id, goal_id, goal_id, goal_id, goal_name))
 
 
 @admin.register(GoalState)
--- frontend/views.py
+++ frontend/views.py
@@ -14,10 +14,20 @@ def draw_battle_goal(request):
 	goal = ActorGoal.objects.filter(actor=actor).order_by('batch').last()
 	if goal is None:
 		return HttpResponse('No actor added yet!!!')
-	img_path = goal.goal_img_path
+	if request.method == 'POST':
+		selected = int(request.POST['selected'])
+		# print('selected', selected)
+		if selected == 1:
+			goal.selected_goal_img_path = goal.drawn_goal_1_img_path
+		if selected == 2:
+			goal.selected_goal_img_path = goal.drawn_goal_2_img_path
+		goal.save()
+	img_path = goal.selected_goal_img_path
+	img_path_1 = goal.drawn_goal_1_img_path
+	img_path_2 = goal.drawn_goal_2_img_path
 	state = GoalState.objects.filter(for_batch=goal.batch).first()
 	if state is None:
 		distributed = False
 	else:
 		distributed = state.distributed
-	return render(request, 'draw_battle_goal.html', {'img_path': img_path, 'distributed': distributed})
+	return render(request, 'draw_battle_goal.html', {'img_path': img_path, 'img_path_1': img_path_1, 'img_path_2': img_path_2, 'distributed': distributed})
