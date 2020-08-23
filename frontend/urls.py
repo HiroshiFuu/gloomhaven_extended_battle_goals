24df6512c1fef9d2c3a5249ef643794d80572f26
Feng Hao <hiroushifuu@outlook.com>
2020-08-24 07:48:10 +0800

    [fix] refresh page form submission

--- frontend/urls.py
+++ frontend/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     # re_path(r'^.*\.html', views.pages, name='pages'),
 
     path('draw_battle_goal/', views.draw_battle_goal, name='draw_battle_goal'),
+    path('selected_battle_goal/', views.selected_battle_goal, name='selected_battle_goal'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 
 from .models import ActorGoal
@@ -10,24 +13,40 @@ from .constants import BATTLE_GOALS
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def draw_battle_goal(request):
-	actor = request.user
-	goal = ActorGoal.objects.filter(actor=actor).order_by('batch').last()
-	if goal is None:
-		return HttpResponse('No actor added yet!!!')
-	if request.method == 'POST':
-		selected = int(request.POST['selected'])
-		# print('selected', selected)
-		if selected == 1:
-			goal.selected_goal_img_path = goal.drawn_goal_1_img_path
-		if selected == 2:
-			goal.selected_goal_img_path = goal.drawn_goal_2_img_path
-		goal.save()
-	img_path = goal.selected_goal_img_path
-	img_path_1 = goal.drawn_goal_1_img_path
-	img_path_2 = goal.drawn_goal_2_img_path
-	state = GoalState.objects.filter(for_batch=goal.batch).first()
-	if state is None:
-		distributed = False
-	else:
-		distributed = state.distributed
-	return render(request, 'draw_battle_goal.html', {'img_path': img_path, 'img_path_1': img_path_1, 'img_path_2': img_path_2, 'distributed': distributed})
+    actor = request.user
+    goal = ActorGoal.objects.filter(actor=actor).order_by('batch').last()
+    if goal is None:
+        return HttpResponse('No actor added yet!!!')
+    if request.method == 'POST':
+        selected = int(request.POST['selected'])
+        # print('selected', selected)
+        if selected == 1:
+            goal.selected_goal_img_path = goal.drawn_goal_1_img_path
+        if selected == 2:
+            goal.selected_goal_img_path = goal.drawn_goal_2_img_path
+        goal.save()
+    img_path = goal.selected_goal_img_path
+    img_path_1 = goal.drawn_goal_1_img_path
+    img_path_2 = goal.drawn_goal_2_img_path
+    state = GoalState.objects.filter(for_batch=goal.batch).first()
+    if state is None:
+        distributed = False
+    else:
+        distributed = state.distributed
+    return render(request, 'draw_battle_goal.html', {'img_path': img_path, 'img_path_1': img_path_1, 'img_path_2': img_path_2, 'distributed': distributed})
+
+
+@login_required(login_url='/accounts/login/')
+def selected_battle_goal(request):
+    if request.method == 'POST':
+        actor = request.user
+        goal = ActorGoal.objects.filter(actor=actor).order_by('batch').last()
+        selected = int(request.POST['selected'])
+        # print('selected', selected)
+        if selected == 1:
+            goal.selected_goal_img_path = goal.drawn_goal_1_img_path
+        if selected == 2:
+            goal.selected_goal_img_path = goal.drawn_goal_2_img_path
+        goal.save()
+    # return HttpResponseRedirect(reverse('frontend:draw_battle_goal'))
+    return redirect('frontend:draw_battle_goal', permanent=True)
